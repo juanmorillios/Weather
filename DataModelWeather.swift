09742fbcb6bdd99996174c5f4a487e973473a7da
Juan Morillo <juanmorillios@gmail.com>
2016-11-27 01:02:39 +0100

    Create Model Of Weather

---
+++ DataModelWeather.swift
@@ -0,0 +1,20 @@
+//
+//  DataModelWeather.swift
+//  Weather
+//
+//  Created by Juan Morillo on 27/11/16.
+//  Copyright © 2016 Juan Morillo. All rights reserved.
+//
+
+import Foundation
+
+class DataModel {
+
+  private var _date: Double?
+  private var _temp: String?
+  private var _location: String?
+  private var _weather: String?
+  
+  typealias JSONStandard = Dictionary<String, Any>
+ 
+}
